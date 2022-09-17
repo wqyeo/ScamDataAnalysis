500e554ae809ca5057c3f00b7e75b41c3db14591
Kaynn Cahya <wqyeo@outlook.com>
2022-09-17 19:13:08 +0800

    Made basic static UI

--- DataCrawler/DataCrawlerApp.py
+++ DataCrawler/DataCrawlerApp.py
@@ -1,8 +1,10 @@
 import PySimpleGUI as sg
 
+from DataCrawler.View.CrawlerView import *
+
 class DataCrawlerApp:
     def __init__(self) -> None:
-        self.window = sg.Window(title = "Data Crawler", layout=[[]], margins=(50, 20))
+        self.window = sg.Window(title = "Data Crawler", layout= CreateCrawlerViewLayout(), margins=(50, 20))
         self.isOpen = True
         pass
 
@@ -10,3 +12,5 @@ class DataCrawlerApp:
         event, value = self.window.read()
         if event == sg.WINDOW_CLOSED:
             self.isOpen = False
+        elif event == START_CRAWL:
+            print("User requested to crawl")
